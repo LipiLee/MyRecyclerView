fec418d34d450eed65bf5bcde4f033c6e4e2fa39
Lipi C.H. Lee <lipisoft@gmail.com>
2017-10-15 09:15:36 +0900

    Add a Handler on the UI Thread

--- app/src/main/java/com/lipisoft/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/lipisoft/myrecyclerview/MainActivity.kt
@@ -1,34 +1,45 @@
 package com.lipisoft.myrecyclerview
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.Message
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.my_recycler_view
+import kotlinx.android.synthetic.main.activity_main.*
 import java.text.DateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val update = 1
+    private lateinit var handler : Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val myDataset : MutableList<Packet> = mutableListOf()
+        val list : MutableList<Packet> = mutableListOf()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         my_recycler_view.setHasFixedSize(false)
         my_recycler_view.layoutManager = LinearLayoutManager(this)
 
-        my_recycler_view.adapter = MyAdapter(myDataset)
+        my_recycler_view.adapter = MyAdapter(list)
 
-        val handler = Handler(Looper.getMainLooper()) {
-
+        handler = object : Handler(Looper.getMainLooper()) {
+            override fun handleMessage(msg: Message?) {
+                if (msg != null) {
+                    when(msg.what) {
+                        update -> my_recycler_view.adapter.notifyDataSetChanged()
+                        else -> super.handleMessage(msg)
+                    }
+                }
+            }
         }
+
         Thread(Runnable
             {
                 while (true) {
-                    myDataset.add(Packet(DateFormat.getDateTimeInstance().format(Date()), "TCP"))
-                    my_recycler_view.adapter.notifyDataSetChanged()
+                    list.add(Packet(DateFormat.getDateTimeInstance().format(Date()), "TCP"))
+                    handler.obtainMessage(update).sendToTarget()
                     Thread.sleep(500)
                 }
             }
--- app/src/main/java/com/lipisoft/myrecyclerview/MyAdapter.kt
+++ app/src/main/java/com/lipisoft/myrecyclerview/MyAdapter.kt
@@ -4,10 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 
-class MyAdapter(val mDataSet: MutableList<Packet>) : RecyclerView.Adapter<MyViewHolder> (){
-    init {
-//        registerAdapterDataObserver(MyAdapterDataObserver(this))
-    }
+class MyAdapter(private val list: MutableList<Packet>) : RecyclerView.Adapter<MyViewHolder> (){
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MyViewHolder? {
         if (parent != null) {
             return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false))
@@ -17,12 +14,12 @@ class MyAdapter(val mDataSet: MutableList<Packet>) : RecyclerView.Adapter<MyView
 
     override fun onBindViewHolder(holder: MyViewHolder?, position: Int) {
         if (holder != null) {
-            holder.date.text = mDataSet[position].date
-            holder.protocol.text = mDataSet[position].protocol
+            holder.date.text = list[position].date
+            holder.protocol.text = list[position].protocol
         }
     }
 
     override fun getItemCount(): Int {
-        return mDataSet.size
+        return list.size
     }
 }
